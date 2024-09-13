a35e394ee569523cf825fd9b2b174e7fa3042f18
Yinxuan Wu <35893137+halfwater223@users.noreply.github.com>
2024-09-13 13:01:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+"""
+Cache Simulation Project
+Author: Yinxuan Wu, Ning Wang
+Institution: Network Science Lab, ECE Department, UMass Amherst
+License: MIT License
+
+MIT License
+
+Copyright (c) 2024 Yinxuan Wu, Ning Wang
+"""
+
 from simulation import SimulationEnvironment
 
 if __name__ == '__main__':
